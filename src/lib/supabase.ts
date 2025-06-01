e4a35ee2972b83f33f33bf7eec3507bfb73e7eb1
Prince-023 <princemoliya29@gmail.com>
2025-06-01 10:44:11 +0530

    cimit message

---
+++ src/lib/supabase.ts
@@ -0,0 +1,25 @@
+console.log('URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log('ANON KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
+}
+
+if (!supabaseKey) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export interface Image {
+  id: string;
+  url: string;
+  public_id: string;
+  filename: string;
+  created_at: string;
+} 
